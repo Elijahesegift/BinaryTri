c3063f48710bf0bfcfe56aa0ad02ec1825669991
Elijah Taylor <ElijahTaylor77@outlook.com>
2020-02-24 14:13:49 -0800

    Add project files.

---
+++ BinaryTri/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Program
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            int j, lastInt = 0, input;
+            Console.WriteLine("Enter the Number of Rows : ");
+            input = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= input; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    if (lastInt == 1)
+                    {
+                        Console.Write("0");
+                        lastInt = 0;
+                    }
+                    else if (lastInt == 0)
+                    {
+                        Console.Write("1");
+                        lastInt = 1;
+                    }
+                }
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
